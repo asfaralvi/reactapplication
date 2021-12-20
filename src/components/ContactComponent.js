2a1b10a4ebca92d4cfc8999a2e434fa19df9ef88
asfaralvi <masfar333@outlook.com>
2021-12-21 00:17:33 +0500

    redux submit comments assignment 3

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -27,6 +27,7 @@ class Contact extends Component {
         console.log('Current State is: ' + JSON.stringify(values));
         alert('Current State is: ' + JSON.stringify(values));
         
+        
     }
 
 
