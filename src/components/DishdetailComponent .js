4256f8fe84940d70f327c3ad42b6a8f9071e1e79
asfaralvi <masfar333@outlook.com>
2021-12-25 16:38:07 +0500

    fetch from server react redux

--- src/components/DishdetailComponent .js
+++ src/components/DishdetailComponent .js
@@ -4,6 +4,8 @@ import { Card, CardImg, CardText, CardBody,
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
+
 
 
 
@@ -111,7 +113,7 @@ class CommentForm extends Component{
             return(
                 <div className="col-md-5 m-1">
                 <Card>
-                    <CardImg src={dish.image} alt={dish.name} />
+                <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                     <CardBody>
                       <CardTitle>{dish.name}</CardTitle>
                       <CardText>{dish.description}</CardText>
